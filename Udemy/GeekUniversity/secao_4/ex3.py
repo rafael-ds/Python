b3dbfeef26e7dfef7d4b2bc67245f3d519051343
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2020-10-18 16:08:32 -0300

    Refatorado com while
    Codigo refatorado com um loop while.
    Pros -> O codigo ficou menos verboso.
    Contra -> Implicou em mais linhas de codigo.

--- Udemy/GeekUniversity/secao_4/ex3.py
+++ Udemy/GeekUniversity/secao_4/ex3.py
@@ -1,8 +1,19 @@
 # Peça ao usuario para digitar 3 numero e retorne a somas de ambos
 
-num1 = int(input('Entre com um numero: '))
+"""num1 = int(input('Entre com um numero: '))
 num2 = int(input('Entre com um numero: '))
 num3 = int(input('Entre com um numero: '))
 
 soma = num1 + num2 + num3
-print(soma)
+print(soma)"""
+
+nums = []
+cont = 0
+
+while cont < 3:
+    numeros = int(input('Entre com um numero: '))
+    nums.append(numeros)
+    cont += 1
+
+soma = sum(nums)
+print(f'A somas dos numeros {nums} é: {soma}')
