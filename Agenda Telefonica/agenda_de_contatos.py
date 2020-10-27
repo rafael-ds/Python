ab30ae20b68ad164085daefa55c376833636e9a3
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2020-10-27 08:36:46 -0300

    Implementando as opc 4 e opc 5
    Emplemetei a logica das opc 4 e 5, porem a opoção 4 esta causando erro Erro ao tentar excluir um item da lista
    -> list assignment index out of range

--- Agenda Telefonica/agenda_de_contatos.py
+++ Agenda Telefonica/agenda_de_contatos.py
@@ -1,17 +1,10 @@
 # Agenda de contatos
-
 import time
 
+contatos = []
+
 print('==' * 20 + ' Agenda Pessoal ' + '==' * 20 + '\n')
-
-print('Deseja acessar sua agenda? ')
-
-contatos = [
-    {'nome': 'Mae', 'tel.': 90165634, 'E-mail': 'dinha@gmail.com'},
-    {'nome': 'Raquel', 'tel.': 2254178, 'E-mail': 'nat@gmail.com'}
-]
-
-acessar = str(input('(s/n):'))
+acessar = str(input('Deseja acessar sua agenda?\n(s/n):'))
 
 while acessar == 's':
     print('=' * 100)
@@ -34,7 +27,7 @@ while acessar == 's':
         tel = int(input('Entre com o telefone do contato: '))
         email = str(input('Entre com o e-mail do contato: '))
 
-        add_contatos = {'nome': nome, 'tel.': tel, 'E-mail': email}
+        add_contatos = {'nome': nome, 'tel.': tel, 'email': email}
         contatos.append(add_contatos)
 
         time.sleep(1.5)
@@ -45,7 +38,7 @@ while acessar == 's':
         for pos in contatos:
             print(f'{contatos.index(pos)}: {pos}')
 
-        posicao = int(input('\nEntre com a posição do contato: '))
+        posicao = int(input('\nEntre com a posição do contato:\n '))
         print(f'{posicao}: {contatos[posicao]}')
 
         alterar = str(input('Deseja alterar esse contato? s/n:\n '))
@@ -55,15 +48,33 @@ while acessar == 's':
             tel = int(input('Entre com o telefone do contato: '))
             email = str(input('Entre com o e-mail do contato: '))
 
-            contatos[posicao] = {'nome': nome, 'tel.': tel, 'E-mail': email} # altera o contato na posição escolida
+            contatos[posicao] = {'nome': nome, 'tel.': tel, 'E-mail': email}  # altera o contato na posição escolida
             time.sleep(1.2)
             print('Contato atualizado com sucesso!\n')
 
-
     elif opc == '4':
-        for i in contatos:
-            print(contatos)
-            # Nao encontrando alogica para a remoção
+        # Erro ao tentar excluir um item da lista
+        # list assignment index out of range
+        for pos in contatos:
+            print(f'{contatos.index(pos)}: {pos}')
+
+        posicao = int(input('Entre com a posição do contato: '))
+
+        alterar = str(input('Deseja realmente excluir esse contato? '))
+
+        if alterar == 's':
+            for i in range(0, len(contatos)):
+                contatos[i] = contatos.pop(posicao)
+            print(f'{contatos}\nContatos excluido com sucesso!')
+
+    else:
+        if opc == '5':
+            sair = str(input('Desaja sair da agenda? (s/n): '))
+
+            if sair == 's':
+                print('Saindo da agenda...')
+                time.sleep(1.5)
+                break
 
 if acessar == 'n':
     print('Saindo da agenda...')
