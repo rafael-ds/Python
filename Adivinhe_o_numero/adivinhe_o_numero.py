5058637609a5a79a3db702d2ebae5a12d49db79e
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2020-11-02 08:14:50 -0300

    Mini jogo de adivinhar um numero de 1 a 100
    Usei para treinar alguns conseito de funções como a chamada global de uma variavel.

---
+++ Adivinhe_o_numero/adivinhe_o_numero.py
@@ -0,0 +1,47 @@
+# Jogo de adivinhação de numero
+
+from random import choice
+import time
+
+com = choice(range(1, 100))
+
+print('='*30 + ' Adivinhe o numero ' + '='*30)
+print('Buscando por um numero entre 1 e 100...')
+time.sleep(1.5)
+print('Numero encontrado. Tente adivinha-lo.\n')
+
+
+def num():
+    """
+    Uma pequena função que tem como objetivo a comparação da variavel(int) COM e a variavel(int) JOGADOR
+    :return: Não possui retorno
+    """
+
+    tentaivas = 0
+    pontuacao = 100
+
+    global com  # chamada global da variavel n para dentro do escopo da função
+
+    while tentaivas < 10:
+
+        jogador = int(input('Entre com o numero: '))
+
+        if jogador < com:
+            pontuacao -= 5
+            print(f'Numero errado. Tente um valor maior.\nSua Pontação é {pontuacao}\n')
+
+        elif jogador > com:
+            pontuacao -= 5
+            print(f'Numero errado. Tente um valor menor. Sua Pontação é {pontuacao}\n')
+        else:
+            print(f'Parabéns você acertou o numero! Vecê fez {pontuacao} pontos\n')
+            break
+
+        tentaivas += 1
+
+    if tentaivas >= 10:
+        print(f'Suas tentativas acabaram. Vecê fez {pontuacao} pontos')
+        print(f'O numero sortiado era {n}')
+
+
+num()
