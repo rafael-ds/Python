8436db407a25b6b65c25b297d36a4ae3ff1e6d23
Rafael Silva <73002086+rafael-ds@users.noreply.github.com>
2021-03-04 17:54:05 -0300

    Update gerador_de_senha.py
    Desenvolvido a logica que verifica a existência de um cabeçalho. Randomização realocada para " with... as gerar". Desenvolvido um menu "while infinito".

--- gerado_de_senha/gerador_de_senha.py
+++ gerado_de_senha/gerador_de_senha.py
@@ -1,20 +1,80 @@
 import string
+import csv
 from random import choice
-
 
 letra = string.ascii_letters
 numeros = string.digits
 caracteres = '_@$&'
 
-comcat = letra + numeros + caracteres
+concat = letra + numeros + caracteres
 temp = []
 
-# Randomização dos elementos
-cont = 0
-while cont < 8:
-    randomizar = choice(comcat)
-    temp.append(randomizar)
-    cont += 1
 
-# Convertendo a lista temp em string
-convecao_str = ''.join(temp)
+def gerar_senha():
+    """
+    Função geradora de senha
+    :return:
+    """
+    with open('gerador.csv', 'a', encoding='utf-8', newline='') as gerar:
+        cabecalho = ['Instituição', 'Senha']
+        escrever = csv.DictWriter(gerar, fieldnames=cabecalho)
+        if gerar.tell() == 0:  # Verifica se existe algo escrito na primeira linha
+            escrever.writeheader()
+
+        while True:
+            entrada = input('Digite o nome da instituição ou (1) Para sair: - ').title()
+            if entrada != '1':
+                # Randomização dos elementos
+                cont = 0
+                while cont < 8:
+                    randomizar = choice(concat)
+                    temp.append(randomizar)
+                    cont += 1
+
+                gerador = ''.join(temp)  # Concatenando as strings da lista temp
+                escrever.writerow({'Instituição': entrada, 'Senha': gerador})
+                del temp[:]  # Limpando a lista para qua não haja concatecação na gravação
+            else:
+                break
+
+
+def ver_senhas():
+    with open('gerador.csv', 'r', newline='', encoding='utf-8') as abrir:
+        ler = csv.DictReader(abrir)
+        for linhas in ler:
+            print(linhas)
+
+
+def buscar_senha():
+    with open('gerador.csv', 'r', newline='', encoding='utf-8') as abrir:
+        ler = csv.reader(abrir)
+        buscar = input('Digite o nome da instituição: \n').title()
+        for i in ler:
+            if i[0] == buscar:
+                print('Instituição | Senha')
+                print(i)
+
+
+# Menu
+while True:
+    print('=' * 25 + ' GERADOR DE SENHA ' + '=' * 25)
+    print('1 - Gerar senha | '
+          '2 - Ver senhas | '
+          '3 - Buscar por senha | '
+          '4 - Sair')
+
+    opc = input('Entre com a opção desejada: ')
+
+    if opc == '1':
+        gerar_senha()
+
+    elif opc == '2':
+        ver_senhas()
+
+    elif opc == '3':
+        buscar_senha()
+
+    elif opc == '4':
+        print('saindo do gerador senha')
+        break
+
